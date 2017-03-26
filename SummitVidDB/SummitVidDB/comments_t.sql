61ae47956c1069f21263120d48a4e67e26dbe66c
MaameAJ <maame.ab.apenteng@gmail.com>
2017-03-25 20:33:15 -0400

    cleaned up sql, added tags folder

--- SummitVidDB/SummitVidDB/comments_t.sql
+++ SummitVidDB/SummitVidDB/comments_t.sql
@@ -1,1 +1,20 @@
-﻿
+﻿/*
+The database must have a MEMORY_OPTIMIZED_DATA filegroup
+before the memory optimized object can be created.
+
+The bucket count should be set to about two times the 
+maximum expected number of distinct values in the 
+index key, rounded up to the nearest power of two.
+*/
+
+CREATE TABLE [dbo].[comments_t]
+(
+	[Id] INT NOT NULL PRIMARY KEY NONCLUSTERED HASH WITH (BUCKET_COUNT = 131072), 
+    [Author_Id] INT NOT NULL, 
+    [Video_Id] INT NOT NULL, 
+    [Comment] NVARCHAR(50) NOT NULL, 
+    [Associated_Frame_Seconds] INT NULL, 
+    [Created] DATETIME NOT NULL,
+	CONSTRAINT [FK_Users_Comments] FOREIGN KEY ([Author_Id]) REFERENCES [dbo].[users_t]([Id]),
+	CONSTRAINT [FK_Videos_Comments] FOREIGN KEY ([Video_Id]) REFERENCES [dbo].[video_t]([Id]),  
+) WITH (MEMORY_OPTIMIZED = ON)
---
+++ SummitVidDB/SummitVidDB/video_tags_t.sql
@@ -0,0 +1,17 @@
+﻿/*
+The database must have a MEMORY_OPTIMIZED_DATA filegroup
+before the memory optimized object can be created.
+
+The bucket count should be set to about two times the 
+maximum expected number of distinct values in the 
+index key, rounded up to the nearest power of two.
+*/
+
+CREATE TABLE [dbo].[video_tags_t]
+(
+	[Id] INT NOT NULL PRIMARY KEY NONCLUSTERED HASH WITH (BUCKET_COUNT = 131072), 
+    [Video_Id] INT NULL, 
+    [Tag_Id] INT NULL,
+	CONSTRAINT [FK_VideoTag_Video] FOREIGN KEY ([Video_Id]) REFERENCES [dbo].[video_t]([Id]),
+	CONSTRAINT [FK_VideoTag_Tag] FOREIGN KEY ([Tag_Id]) REFERENCES [dbo].[tags_t]([Id])  
+) WITH (MEMORY_OPTIMIZED = ON)
